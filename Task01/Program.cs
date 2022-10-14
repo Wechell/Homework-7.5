93e4cbb133bb0b0f569034d9d169ad9ddbe649ad
muxin <muxin@DESKTOP-87GTR2S>
2022-10-14 21:04:46 +0300

    Добавьте файлы проекта.

---
+++ Task01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class MainClass
+{
+    static string[] SplitWordsOfSentence(string Sentence)
+    {
+        string[] wordsOfSentence = Sentence.Split(' ');
+        return wordsOfSentence;
+    }
+
+    static void PrintWords(string[] words)
+    {
+        for (int i = 0; i < words.Length; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите предложение, в котором слова разделены пробелами: ");
+
+        string userSentence = Console.ReadLine();
+        string[] words = SplitWordsOfSentence(userSentence);
+        
+        PrintWords(words);
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class MainClass
+{
+    static string[] SplitSentenceOnWords(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        return words;
+    }
+
+    static string ReverseWords(string someSentence)
+    {
+        string[] words = SplitSentenceOnWords(someSentence);
+        string reverseWords = "";
+
+        for (int i = words.Length - 1; i >= 0; i--)
+        {
+            reverseWords += words[i] + " ";
+        }
+        return reverseWords;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите предложение, в котором слова разделены пробелами: ");
+
+        string userSentence = Console.ReadLine();
+
+        string reverseSentence = ReverseWords(userSentence);
+
+        Console.WriteLine(reverseSentence);
+    }
+}
